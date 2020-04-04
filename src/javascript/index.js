5cc9170e0f5c37d3c328b822a2283ca57ca14feb
Michał Fal <michfal86@gmail.com>
2020-04-04 16:18:48 +0200

    gallery api change

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,18 +1,20 @@
 import '../sass/style.scss';
 import '../template/template.html';
 
-console.log("hi")
-
 
 const galleryImages = document.querySelector('.j-gallery_images')
 
 //get ID's of all user photos
 
-fetch('https://api.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&api_key=874fa0262a2884c9b12c0e6a032030fc&user_id=146495496@N08&format=json&nojsoncallback=1')
+const key = "74f4e44187e9b604246cfdba369c24a6";
+const userId = "187676962@N05";
+
+fetch(`https://api.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&api_key=${key}&user_id=${userId}&format=json&nojsoncallback=1`)
+
 
 .then(resp => resp.json())
 .then(data => {
-    getAllPhotos(getPhotoIds(data))  
+    getAllPhotos(getIdsOfPhoto(data))  
 })
 
 
@@ -21,14 +23,15 @@ function getAllPhotos(photoIds) {
     photoIds.forEach((id) => getPhotoById(id))
 }
 
-function getPhotoIds(data) {
+function getIdsOfPhoto(data) {
     const pulledValues = data;
     const photos = (pulledValues.photos).photo
     const keys = Object.keys(pulledValues)
     //console.log(photos)
     let photoIds = []
+    console.log(photoIds)
     photos.forEach((e) => photoIds.push(e.id))
-    //console.log(photoIds)
+    
     return photoIds
 }    
 
@@ -36,7 +39,7 @@ function getPhotoIds(data) {
 //pulls photo according to ID and place it on website
 
 function getPhotoById(id) {
-    fetch(`https://api.flickr.com/services/rest/?method=flickr.photos.getSizes& api_key=874fa0262a2884c9b12c0e6a032030fc&user_id=146495496@N08&  photo_id=${id}&format=json&nojsoncallback=1`)
+    fetch(`https://api.flickr.com/services/rest/?method=flickr.photos.getSizes& api_key=${key}&user_id=${userId}&  photo_id=${id}&format=json&nojsoncallback=1`)
 
     .then(resp => resp.json())
     .then(data => {
@@ -47,7 +50,7 @@ function getPhotoById(id) {
 function getPhotoSizes(data, sizeIndex) {    
     const photoData = data;
     const photoSizes = photoData.sizes;
-    console.log(photoSizes)
+    //console.log(photoSizes)
     //photoSizes.size gets array of objects cotaining: label, dimentions and source of picture
     //console.log(photoSizes.size)   //uncoment to see available sizes
     //photoSizes.size[x].source get's source of image according to it's index
