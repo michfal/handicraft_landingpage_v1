9e5c18b5d137a56a95920a8f078500daaca2ad24
Michał Fal <michfal86@gmail.com>
2020-04-27 20:51:01 +0200

    picture display styling

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -4,11 +4,10 @@ import '../template/template.html';
 
 const galleryImages = document.querySelector('.j-gallery_images')
 
-//get ID's of all user photos
-
 const key = "74f4e44187e9b604246cfdba369c24a6";
 const userId = "187676962@N05";
 
+//get ID's of all user photos
 function fetchPhotoIds() { 
     fetch(`https://api.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&api_key=${key}&user_id=${userId}&format=json&nojsoncallback=1`)
 
@@ -18,42 +17,46 @@ function fetchPhotoIds() {
     })
 }
 
+
+//filters raw data
 function filterIdsOfPhoto(data) {
     const pulledValues = data;
     const photos = (pulledValues.photos).photo
     const keys = Object.keys(pulledValues)
     //console.log(photos)
     let photoIds = []
-    console.log(photoIds)
-    photos.forEach((e) => photoIds.push(e.id))
     
+    photos.forEach((e) => photoIds.push(e.id))
+    console.log(photoIds)
     return photoIds
 }
 
 
 function getAllPhotos(photoIds) {
-    photoIds.forEach((id) => getPhotoById(id))
+    photoIds.forEach((id) => getPhotoById(id, 5, createGalleryImage))
 }
 
     
 //pulls photo according to ID and place it on website
-function getPhotoById(id) {
+//as callback uses createGalleryImage for building gallery images or displayImage for displaying big picture
+function getPhotoById(id, size, callback) {
     fetch(`https://api.flickr.com/services/rest/?method=flickr.photos.getSizes& api_key=${key}&user_id=${userId}&  photo_id=${id}&format=json&nojsoncallback=1`)
 
     .then(resp => resp.json())
     .then(data => {
-        createImage(getPhotoSizes(data, 5), id)
+        //console.log(data)
+        callback(getPhotoSizes(data, size), id)       
     })
 }
 
+//get source of image according do size index
 function getPhotoSizes(data, sizeIndex) {    
     const photoData = data;
     const photoSizes = photoData.sizes;
-
     return(photoSizes.size[sizeIndex].source)
 }
 
-function createImage(source, id) {
+function createGalleryImage(source, id) {
     
     const imageContainer = document.createElement("div");
     galleryImages.appendChild(imageContainer);
@@ -68,3 +71,32 @@ function createImage(source, id) {
 }
 
 fetchPhotoIds()
+
+
+//==========================
+
+//galleryImages.addEventListener('click', galleryImageClick)
+
+function galleryImageClick() {
+    // if (event.target.tagName == 'IMG') {
+    //     console.log(event.target.classList.value)
+    //     getPhotoById(id, 5, displayImage)
+    // }
+    //test purpose
+    console.log("creating")
+    let id = 49721768792
+    getPhotoById(id, 10, displayImage)
+}
+
+function displayImage(source, id) {
+    const imageDisplay = document.querySelector('.j-image_display')
+    //create img element
+    const image = document.createElement("img");
+    imageDisplay.appendChild(image);
+    image.src = `${source}`;
+    image.classList.add('image_display__image')
+    //assign class image_display__image
+    //append child image display
+}
+
+galleryImageClick()
